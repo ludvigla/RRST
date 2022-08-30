bf1f1a63cce8503e4b6ce8c1ba576f5d4a783fd7
Ludvig Larsson <ludvig.larsson@scilifelab.se>
2022-08-30 15:28:19 +0200

    Updated repo

---
+++ scripts/mendeley.R
@@ -0,0 +1,27 @@
+genes.files <- setNames(c("https://data.mendeley.com/api/datasets/4w6krnywhn/draft/files/9e85bc60-57e9-482d-8d08-5888d6766932?inline=true&a=eafe3c2d-708d-44a5-b72f-f60b9e158db8",
+                 "https://data.mendeley.com/api/datasets/4w6krnywhn/draft/files/3619912a-46a9-408c-9cb1-9b424e5e3d65?inline=true&a=eafe3c2d-708d-44a5-b72f-f60b9e158db8"),
+                 nm = c("hgenes.tsv", "mgenes.tsv"))
+
+r.files <- setNames(c("https://data.mendeley.com/api/datasets/4w6krnywhn/draft/files/ba1d3836-01b4-4b87-bbc1-c2cc6bc8e161?inline=true&a=eafe3c2d-708d-44a5-b72f-f60b9e158db8",
+              "https://data.mendeley.com/api/datasets/4w6krnywhn/draft/files/6a3c0cf5-34c0-4bcc-baff-dafba8ff55b0?inline=true&a=eafe3c2d-708d-44a5-b72f-f60b9e158db8"),
+              nm = c("CLN_metadata_selections", "SI_metadata_selections"))
+
+sheets.files <- setNames(c("https://data.mendeley.com/api/datasets/4w6krnywhn/draft/files/310cbbcd-1482-474e-a817-ed78535cc80e?inline=true&a=eafe3c2d-708d-44a5-b72f-f60b9e158db8",
+                   "https://data.mendeley.com/api/datasets/4w6krnywhn/draft/files/de4278fc-1662-4d1e-9738-404948b66a42?inline=true&a=eafe3c2d-708d-44a5-b72f-f60b9e158db8",
+                   "https://data.mendeley.com/api/datasets/4w6krnywhn/draft/files/86da3d06-a2c6-4ba5-a0a7-f39d498356e2?inline=true&a=eafe3c2d-708d-44a5-b72f-f60b9e158db8"),
+                   nm = c("curated_RNA_rescue_sample_metadata.xlsx", "Visium_Human_Transcriptome_Probe_Set_v1.0_GRCh38-2020-A.csv", "Visium_Mouse_Transcriptome_Probe_Set_v1.0_mm10-2020-A.csv"))
+
+bone.annotation.files <- setNames(c("https://data.mendeley.com/api/datasets/4w6krnywhn/draft/files/0b2a0051-4ea2-4fdb-8dcf-7acfb4ec1191?inline=true&a=eafe3c2d-708d-44a5-b72f-f60b9e158db8",
+                           "https://data.mendeley.com/api/datasets/4w6krnywhn/draft/files/a5963743-8074-4c57-b4c5-026ba716fc0f?inline=true&a=eafe3c2d-708d-44a5-b72f-f60b9e158db8",
+                           "https://data.mendeley.com/api/datasets/4w6krnywhn/draft/files/b78c7b5c-693c-4cba-bb72-b494ad8ad4ed?inline=true&a=eafe3c2d-708d-44a5-b72f-f60b9e158db8",
+                           "https://data.mendeley.com/api/datasets/4w6krnywhn/draft/files/acbf1c46-f123-4003-9a2a-8aed6492477b?inline=true&a=eafe3c2d-708d-44a5-b72f-f60b9e158db8"),
+                           nm = c("mouse growth plateA1 p4.csv", "mouse growth plateB1 p4.csv", "mouse growth plateC1 p11.csv", "mouse growth plateD1 p11.csv"))
+
+spaceranger.files <- setNames(c("https://data.mendeley.com/api/datasets/4w6krnywhn/draft/files/8524fc43-c484-4593-889f-4ca72195139c?inline=true&a=eafe3c2d-708d-44a5-b72f-f60b9e158db8",
+                                "https://data.mendeley.com/api/datasets/4w6krnywhn/draft/files/0992dff5-8f56-472a-bc66-10b5acffe700?inline=true&a=eafe3c2d-708d-44a5-b72f-f60b9e158db8",
+                                "https://data.mendeley.com/api/datasets/4w6krnywhn/draft/files/1e298325-38ea-4ae2-8b52-184847a442ab?inline=true&a=eafe3c2d-708d-44a5-b72f-f60b9e158db8",
+                                "https://data.mendeley.com/api/datasets/4w6krnywhn/draft/files/6e786948-df64-460a-a0a6-452c4d66089b?inline=true&a=eafe3c2d-708d-44a5-b72f-f60b9e158db8",
+                                "https://data.mendeley.com/api/datasets/4w6krnywhn/draft/files/1867326b-5fe2-4303-84d8-c6001615c33f?inline=true&a=eafe3c2d-708d-44a5-b72f-f60b9e158db8",
+                                "https://data.mendeley.com/api/datasets/4w6krnywhn/draft/files/4cfc24e9-a7e4-4f0c-94ce-608d2df6dfee?inline=true&a=eafe3c2d-708d-44a5-b72f-f60b9e158db8",
+                                "https://data.mendeley.com/api/datasets/4w6krnywhn/draft/files/6c8cabcc-ef67-4cb4-afcc-77babad09807?inline=true&a=eafe3c2d-708d-44a5-b72f-f60b9e158db8"),
+                              nm = paste0(c("childbraintumor", "colon", "lung", "mousebone", "mousebrain", "prostatecancer", "smallintestine"), ".tar.gz"))
